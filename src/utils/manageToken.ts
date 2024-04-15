0e22b5a7f6228bda939564476c4da7db0734cb6e
kimwanyoung <dhks2869@gmail.com>
2024-04-15 17:28:57 +0900

    refactor: ManageToken 가독성 향상을 위해 리팩토링

--- src/utils/manageToken.ts
+++ src/utils/manageToken.ts
@@ -7,20 +7,33 @@ export class ManageToken {
         try {
             await this.requestToken(`${HOST}/auth/token/access`, true);
         } catch (error) {
+            console.error('access token 재발급에 실패하였습니다. refreshToken재발급 후 다시 요청합니다.');
+            await this.retryRotateToken();
+        }
+    }
+
+    private static async retryRotateToken() {
+        try {
             await this.requestToken(`${HOST}/auth/token/refresh`, false);
             await this.requestToken(`${HOST}/auth/token/access`, true);
+        } catch (error) {
+            console.error(error);
+            throw new Error('토큰 재발급에 실패하였습니다.');
         }
     }
 
     private static async requestToken(url: string, isAccessToken: boolean) {
         const tokenType = isAccessToken ? 'accessToken' : 'refreshToken';
-        const response = await axios.post(url, {}, {
-            headers: {
-                Authorization: `Bearer ${this.refreshToken}`
-            }
-        });
+        const headers = this.createAuthorizationHeader();
+        const response = await axios.post(url, {}, { headers });
 
         localStorage.setItem(tokenType, response.data[tokenType]);
+    }
+
+    private static createAuthorizationHeader() {
+        return {
+            Authorization: `Bearer ${this.refreshToken}`
+        }
     }
 
     private static get refreshToken() {
